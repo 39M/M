6bc56cccbe9a316829d2dcf6abbf34d81ac5dc11
39M <xwszc7@gmail.com>
2017-04-12 23:38:11 +0800

    Basic Class Define

---
+++ Client/Assets/Scripts/GameManager.cs
@@ -0,0 +1,86 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Newtonsoft.Json;
+
+public class GameManager : MonoBehaviour
+{
+
+    void Start()
+    {
+        string jsonString = new Music
+        {
+            title = "title",
+            artist = "artist",
+            audioFilename = "audio.mp3",
+            previewTime = 0f,
+            soundEffectFilename = "sound.wav",
+            bannerFilename = "banner.png",
+            beatmapList = new List<Beatmap>
+            {
+                new Beatmap
+                {
+                    creator = "creator",
+                    version = "1.0",
+                    difficulty = 1,
+                    difficultyName = "Easy",
+                    difficultyDisplayColor = new SimpleColor(Color.green),
+                    noteList = new List<Note>
+                    {
+                        new Note
+                        {
+                            type = NoteType.Hit,
+                            time = 0f,
+                            speed = 1f,
+                            color = new SimpleColor(Color.grey)
+                        },
+                        new Note
+                        {
+                            type = NoteType.Slide,
+                            time = 1f,
+                            speed = 2f,
+                            color = new SimpleColor(Color.white)
+                        }
+                    }
+                },
+                new Beatmap
+                {
+
+                    creator = "creator",
+                    version = "1.0",
+                    difficulty = 5,
+                    difficultyName = "Normal",
+                    difficultyDisplayColor = new SimpleColor(Color.blue),
+                    noteList = new List<Note>
+                    {
+                        new Note
+                        {
+                            type = NoteType.Hit,
+                            time = 0f,
+                            speed = 1f,
+                            color = new SimpleColor(Color.grey)
+                        },
+                        new Note
+                        {
+                            type = NoteType.Slide,
+                            time = 1f,
+                            speed = 2f,
+                            color = new SimpleColor(Color.white)
+                        }
+                    }
+                }
+            }
+        }.ToJson();
+
+        Debug.Log(jsonString);
+
+        Music music = Music.FromJson(jsonString);
+
+        Debug.Log(JsonConvert.SerializeObject(music.beatmapList[1], Formatting.Indented));
+    }
+
+    void Update()
+    {
+
+    }
+}
---
+++ Client/Assets/Scripts/Music.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Newtonsoft.Json;
+
+public class Music
+{
+    public string title;
+    public string artist;
+
+    public string audioFilename;
+    public float previewTime;
+
+    public string soundEffectFilename;
+
+    public string bannerFilename;
+
+    public List<Beatmap> beatmapList;
+
+    public static Music FromJson(string json)
+    {
+        return JsonConvert.DeserializeObject<Music>(json);
+    }
+
+    public string ToJson()
+    {
+        return JsonConvert.SerializeObject(this, Formatting.Indented);
+    }
+}
+
+public class Beatmap
+{
+    public string creator;
+    public string version;
+
+    public int difficulty;
+    public string difficultyName;
+    public SimpleColor difficultyDisplayColor;
+
+    public List<Note> noteList;
+}
+
+public enum NoteType
+{
+    Hit, Slide
+}
+
+public class Note
+{
+    public NoteType type;
+    public float time;
+    public float speed;
+    public SimpleColor color;
+}
+
+#region Basic Defination
+
+public class SimpleColor
+{
+    public float r;
+    public float g;
+    public float b;
+    public float a;
+
+    public SimpleColor(Color color)
+    {
+        r = color.r;
+        g = color.g;
+        b = color.b;
+        a = color.a;
+    }
+
+    public Color ToColor()
+    {
+        return new Color(r, g, b, a);
+    }
+}
+
+#endregion
