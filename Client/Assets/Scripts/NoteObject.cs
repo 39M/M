87bdabd49935ecdd0fcd4ecd80f953e76fad3c9b
39M <xwszc7@gmail.com>
2017-04-22 20:51:39 +0800

    add class NoteObject

---
+++ Client/Assets/Scripts/NoteObject.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NoteObject
+{
+    public GameObject gameObject;
+    public Collider collider;
+    public Note note;
+}
