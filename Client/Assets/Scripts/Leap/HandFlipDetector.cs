2dcdbddccf2d779b20d991dc4f0b02ae7e8ae20a
39M <xwszc7@gmail.com>
2017-05-24 19:46:00 +0800

    hand flip detector

---
+++ Client/Assets/Scripts/Leap/HandFlipDetector.cs
@@ -0,0 +1,58 @@
+﻿using Leap.Unity;
+using UnityEngine;
+
+public class HandFlipDetector : Detector
+{
+    public LeapProvider provider;
+    [SerializeField]
+    bool activate;
+    [SerializeField]
+    float handRotationZ;
+    [SerializeField]
+    bool isUp;
+
+    float minAngel;
+    float maxAngel;
+
+    void Start()
+    {
+        provider = FindObjectOfType<LeapProvider>() as LeapProvider;
+        activate = false;
+
+        minAngel = 135;
+        maxAngel = 225;
+    }
+
+    void Update()
+    {
+        if (activate && provider.CurrentFrame.Hands.Count <= 0)
+        {
+            activate = false;
+        }
+
+        if (!activate && provider.CurrentFrame.Hands.Count > 0)
+        {
+            activate = true;
+            handRotationZ = provider.CurrentFrame.Hands[0].Rotation.ToQuaternion().eulerAngles.z;
+            isUp = (minAngel < handRotationZ) && (handRotationZ < maxAngel);
+        }
+
+        if (activate)
+        {
+            float currentHandRotationZ = provider.CurrentFrame.Hands[0].Rotation.ToQuaternion().eulerAngles.z;
+            bool isUpNow = (minAngel < currentHandRotationZ) && (currentHandRotationZ < maxAngel);
+            if (isUpNow != isUp)
+            {
+                isUp = isUpNow;
+                if (isUp)
+                {
+                    Activate();
+                }
+                else
+                {
+                    Deactivate();
+                }
+            }
+        }
+    }
+}
