a23b900070b00d4c6dc46cf558e65780969dbde6
39M <xwszc7@gmail.com>
2017-05-24 10:56:30 +0800

    hand active detector

---
+++ Client/Assets/Scripts/Leap/HandActiveDetector.cs
@@ -0,0 +1,28 @@
+﻿using Leap.Unity;
+
+public class HandActiveDetector : Detector
+{
+    public LeapProvider provider;
+    bool activate;
+
+    void Start()
+    {
+        provider = FindObjectOfType<LeapProvider>() as LeapProvider;
+        activate = false;
+    }
+
+    void Update()
+    {
+        if (activate && provider.CurrentFrame.Hands.Count <= 0)
+        {
+            activate = false;
+            Deactivate();
+        }
+
+        if (!activate && provider.CurrentFrame.Hands.Count > 0)
+        {
+            activate = true;
+            Activate();
+        }
+    }
+}
