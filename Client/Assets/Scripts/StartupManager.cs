8dc60010932024c29c99408f541da5721409f3eb
39M <xwszc7@gmail.com>
2017-05-31 01:56:03 +0800

    basic startup scene

---
+++ Client/Assets/Scripts/StartupManager.cs
@@ -0,0 +1,68 @@
+﻿using DG.Tweening;
+using Leap;
+using Leap.Unity;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class StartupManager : MonoBehaviour
+{
+    public LeapProvider provider;
+
+    void Awake()
+    {
+
+    }
+
+    void Start()
+    {
+        // Init Leap
+        provider = FindObjectOfType<LeapProvider>() as LeapProvider;
+    }
+
+    void Update()
+    {
+        CheckChoice();
+    }
+
+    void CheckChoice()
+    {
+        if (madeChoice)
+        {
+            return;
+        }
+
+        if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            GoToCustomMusic();
+        }
+        else if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            GoToSelectMusic();
+        }
+    }
+
+    bool madeChoice = false;
+    void GoToSelectMusic()
+    {
+        madeChoice = true;
+
+        Utils.FadeOut(1, () =>
+        {
+            SceneManager.LoadScene("SelectMusic");
+        });
+    }
+
+    void GoToCustomMusic()
+    {
+        madeChoice = true;
+
+        Utils.FadeOut(1, () =>
+        {
+            SceneManager.LoadScene("CustomMusic");
+        });
+    }
+}
