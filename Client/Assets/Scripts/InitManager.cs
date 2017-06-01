3e6a75e968b3b70836682b3bab0b806935f424fb
39M <xwszc7@gmail.com>
2017-06-01 19:16:53 +0800

    init scene

---
+++ Client/Assets/Scripts/InitManager.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class InitManager : MonoBehaviour
+{
+    void Start()
+    {
+        StartCoroutine(Utils.WaitAndAction(4, () =>
+        {
+            Utils.FadeOut(1, () =>
+            {
+                SceneManager.LoadScene("Startup");
+            });
+        }));
+    }
+}
